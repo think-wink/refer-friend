ee92a774736b2921f4f0435ac69ef01f2f8d84d9
Kirat <kirat@trybz.com.au>
2023-09-07 14:59:38 +1000

    changed mail class to be more concise

--- app/Mail/ReferrerCreatedMail.php
+++ app/Mail/ReferrerCreatedMail.php
@@ -10,7 +10,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class ReferrerCreated extends Mailable
+class ReferrerCreatedMail extends Mailable
 {
     use Queueable, SerializesModels;
 
