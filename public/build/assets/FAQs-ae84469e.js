6c06c2317559a02c012079eeb91aa44f668c25c5
Kirat <kirat@trybz.com.au>
2023-10-10 08:36:31 +1000

    Build Complete

---
+++ public/build/assets/FAQs-ae84469e.js
@@ -0,0 +1,88 @@
+import{R as u}from"./ReferFriendLayout-ec6781dc.js";import p from"./Item-d08bac88.js";import{_ as m}from"./_plugin-vue_export-helper-c27b6911.js";import{c as r,w as f,r as l,o as i,b as t,n as y,q as v,F as w}from"./app-b3ab0147.js";import"./index-a94b7d0f.js";import"./vue.runtime.esm-bundler-c5bfa865.js";import"./open-closed-89cc2f54.js";import"./use-resolve-button-type-1173025a.js";const g={components:{ReferFriendLayout:u,Item:p},data(){return{icon_style:"w-8 h-8 self-center",items:[{title:"What is a Household Loan?",content:`<div>
+          Please refer to the
+          <a class="text-link" href="https://householdcapital.com.au/"> Household Capital website </a>
+          for more information on Household Loans or call <a class="text-link" href="tel: 1300 057 080">1300 057 080</a> to organise a personalised discussion with a retirement specialist.
+        </div>`},{title:"Am I eligible for a Household Loan?",content:`
+          <div>If you have been referred by a friend, please use our reverse mortgage calculator to see how much you could borrow. You will be required to enter the below information: </div>
+          <ul class="list-disc flex flex-col gap-3 ml-10">
+              <li>Your name, phone number and email so we can send you a summary document of your results</li>
+              <li>The age of the borrower/s (note: the youngest borrower must be aged 60+)</li>
+              <li>Whether your home is a house or apartment</li>
+              <li>Your postcode</li>
+              <li>The estimated value of your home (which needs to be at least $600,000 AUD)</li>
+          </ul>
+          <div>Once you submit that information, if eligible, you’ll see an estimate of the amount you may be able to borrow. </div>
+          <div>If you have any questions regarding our reverse mortgage calculator, call us on <a class= "text-link" href="tel: 1300 169 046">1300 169 046</a>. See how using your Household Capital™ could improve your retirement income and help you Live Well At Home™.  </div>
+        `},{title:"Do I qualify for a reward with Household Capital? ",content:`
+            <div>To qualify for a reward as part of the refer a friend program, you must be a referrer or referee. </div>
+            <div class="font-bold"> Referrer</div>
+            <div>If you have a settled a Household Loan with Household Capital and still hold the loan, you may refer a friend or family member to Household Capital. If your friend settles a loan with Household Capital, you are eligible for a $250 reward.</div>
+            <div class="font-bold">Referee</div>
+            <div>If you have been referred by a friend and have since settled a Household Loan with Household Capital, you are eligible for a $500 reward.</div>
+            <div>
+              More information can be found in the 
+              <a class="text-link" href="/terms"> Terms and Conditions </a>
+              .  
+            </div>
+        `},{title:"How do I receive the reward? ",content:`
+          <div> You will receive an email from The Card Network, containing a link to redeem your reward. You will receive an SMS from The Card Network with prompts to download your Household Capital Digital Prepaid Mastercard onto your smartphone’s digital wallet.  </div>
+          <div> It must be downloaded within three months after receiving the SMS and you will have 12 months to use your Digital Prepaid Mastercard.  </div>
+        `},{title:"What reward am I entitled to? ",content:`
+          <div class="font-bold">Household Loan </div>
+
+          <div>Referrer $250 </div>
+
+          <div>Referee $500 </div>
+
+          <div class="font-bold">Pension Boost Home Equity Access Scheme </div>
+
+          <div>Referrer $50 </div>
+
+          <div>Referee $50 </div>
+        `},{title:"Can I refer more than one friend? ",content:`
+          <div>
+            Yes, if you have a Household Loan you can refer up to 10 friends.
+          </div>
+        `},{title:"I haven’t received my gift card. What do I do?",content:`
+          <div>
+            Please allow up to 3 business days to receive your Digital Prepaid Mastercard. If it has been longer than 3 business days, please contact us at 
+            <a class="text-link" href="#"> customer service </a>
+          </div>
+        `},{title:"How do I use/ activate my Digital Prepaid Mastercard?",content:`
+          <div>Please follow below steps: </div>
+          <ol class="list-decimal flex flex-col gap-3 ml-10">
+            <li>Click the “Redeem Reward” button in the reward email you have received.</li>
+            <li>Follow the instructions supplied by The Card Network via SMS</li>
+            <li>Download the TCN app on your phone</li>
+            <ol class="list-decimal flex flex-col gap-3 ml-5">
+              <li>
+                Apple: 
+                <a class="text-link" href="https://apps.apple.com/au/app/the-card-network/id1481419515"> https://apps.apple.com/au/app/the-card-network/id1481419515 </a>
+              </li>
+              <li>
+                Google:
+                <a class="text-link" href="https://play.google.com/store/apps/details?id=com.tcnpayments.app"> https://play.google.com/store/apps/details?id=com.tcnpayments.app </a>
+              </li>
+            </ol>
+            <li>Add the gift card to your smartphone’s digital wallet;</li>
+            <li>Use tap and go to redeem funds wherever Mastercard is accepted. A PIN also allows your card to be used for online transactions.</li>
+          </ol>
+          `},{title:"Do I need to spend the full value of my Digital Prepaid Mastercard in one transaction or with one retailer? ",content:"No. You can choose how and where you spend the funds on your Digital Prepaid Mastercard. Any remaining balance after a transaction will remain on your Digital Prepaid Mastercard if it has not reached its expiry date.  "},{title:"My phone doesn’t have a digital wallet.",content:"Consider gifting the Digital Prepaid Mastercard to a family member or friend. Before it is downloaded you will need to forward the SMS to their mobile number, then they can then follow the instructions to download the Digital Prepaid Mastercard. "},{title:"How do I check my balance?",content:"You can check your balance via the TCN mobile app."},{title:"If my transaction is greater than the amount on my Digital Prepaid Mastercard, can I pay the rest with cash or credit card?",content:"Yes, you can. You may need to request a split payment from the cashier."},{title:"Can I reload my Digital Prepaid Mastercard?",content:"No, Digital Prepaid Mastercard’s are not reloadable."},{title:"What if my Digital Prepaid Mastercard is not accepted for payment?",content:`
+          <div>Please check the following reasons why your Digital Prepaid Mastercard may not be working: </div>
+          <ol class="list-decimal flex flex-col gap-3 ml-10">
+            <li>The transaction may exceed the balance on the Digital Prepaid Mastercard. Check your balance via the TCN mobile app.</li>
+            <li>Your Digital Prepaid Mastercard has expired. You must use your Digital Prepaid Mastercard within 12 months of activation.</li>
+          </ol>
+          <div>
+            Should your Digital Prepaid Mastercard decline but has available balance and hasn’t expired, please contact The Card Network 
+            <a class="text-link" href="https://thecardnetwork.com.au/pages/contact-us" target="_blank"> https://thecardnetwork.com.au/pages/contact-us </a>
+          </div>
+        `},{title:"What happens if I have not used the entire value of my Digital Prepaid Mastercard and it has expired? ",content:"Once the Digital Prepaid Mastercard has expired, the remaining value will no longer be available."},{title:"What do I do if my Digital Prepaid Mastercard is stolen, lost or is damaged? ",content:"Digital Prepaid Mastercard’s should be treated like cash and kept in a secure place. Unfortunately, we cannot replace a Digital Prepaid Mastercard under any circumstances. "},{title:"I have an additional query, who do I contact?",content:`
+          <div>
+            <span>More information can be found here</span> 
+            <a class="text-link" href="https://householdcapital.com.au/frequently-asked-questions"> https://householdcapital.com.au/frequently-asked-questions/</a>.
+          </div>
+          <div>
+             Otherwise, email us at <a href="mailto:householdcapital@thinkwink.com.au" class="text-link">householdcapital@thinkwink.com.au</a> and our team will assist.
+          </div>
+        `}].map((o,e)=>({open:e===0,id:e,...o}))}},methods:{updateOpen(a){this.items.forEach((o,e)=>{this.items[e].open=!1}),this.items[a].open=!0}}},b={class:"mx-auto py-12"},k={class:"flex justify-center pt-5"},x={class:"text-center text-toolbar w-2/3"},M=t("h3",{class:"text-5xl font-bold font-merry"},"FAQs",-1);function P(a,o,e,I,n,s){const d=l("Item"),c=l("ReferFriendLayout");return i(),r(c,null,{default:f(()=>[t("div",b,[t("div",k,[t("div",x,[M,(i(!0),y(w,null,v(n.items,h=>(i(),r(d,{onToggleOpen:s.updateOpen,state:h,class:"text-left"},null,8,["onToggleOpen","state"]))),256))])])])]),_:1})}const Y=m(g,[["render",P]]);export{Y as default};
